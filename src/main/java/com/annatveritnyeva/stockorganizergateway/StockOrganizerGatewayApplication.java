6f53af446251eb26aa47d9d00a34bde2e5361624
AnnaTveritnyeva <atveretniv@gmail.com>
2022-12-04 21:48:11 +0200

    Initial commit

---
+++ src/main/java/com/annatveritnyeva/stockorganizergateway/StockOrganizerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.annatveritnyeva.stockorganizergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOrganizerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOrganizerGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/annatveritnyeva/stockorganizergateway/StockOrganizerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annatveritnyeva.stockorganizergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrganizerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
