d65a6baceb208a4b885698183c6e60754c04ffa8
AnnaTveritnyeva <atveretniv@gmail.com>
2023-02-04 12:33:28 +0200

    Created LoggingFilter

---
+++ src/main/java/com/annatveritnyeva/stockorganizergateway/LoggingFilter.java
@@ -0,0 +1,25 @@
+package com.annatveritnyeva.stockorganizergateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        logger.info("Path of the request received -> {}, redirected to -> {}", exchange.getRequest().getPath(), route.getUri());
+
+        return chain.filter(exchange);
+    }
+}
